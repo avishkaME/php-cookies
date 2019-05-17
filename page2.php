326b02cc0f558e3115003c18a7fe17bb0e2722de
avishkaME <ameddegoda143@gmail.com>
2019-05-17 22:15:18 +0530

    counting cookies

--- page2.php
+++ page2.php
@@ -2,7 +2,14 @@
   setcookie('username', 'Avi', time() + 3600);
 
   // Delete cookie
-  setcookie('username', 'Avi', time() - 3600);
+  //setcookie('username', 'Avi', time() - 3600);
+
+  //count cookies
+  if (count($_COOKIE) > 0) {
+    echo 'There are '.count($_COOKIE). ' cookies saved<br>';
+  }else {
+    echo 'There are no cookies saved<br>';
+  }
 
   if (isset($_COOKIE['username'])) {
     echo 'User '. $_COOKIE['username'] . ' is set<br>';
